a0dee4ec27f57ac8cb9eb78af76ca63b51db070c
Leo Kirasic <kirasicleo@gmail.com>
2022-11-23 22:50:27 +0100

    Create Item component

--- components/Item.js
+++ components/Item.js
@@ -1,18 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default function Item (props) {
+export default function Item({ item, deleteItem }) {
   return (
-    <View style={styles.item}>
-      <Text style={styles.title}>{props.text}</Text>
-    </View>
+    <TouchableOpacity style={styles.item}>
+      <Text style={styles.text}>{item.text}</Text>
+      <Icon
+        name='remove'
+        size={30}
+        color={'#752a54'}
+        onPress={() => deleteItem(item.id)}
+      ></Icon>
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
-    item: {
-        
-    },
-    title: {
-
-    },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#fefefe',
+    borderBottomColor: '#bab7af',
+    borderBottomWidth: 1,
+  },
+  text: {
+    fontSize: 18,
+  },
 });
+
+Item.propTypes = {
+  text: PropTypes.string,
+  item: PropTypes.obj,
+  id: PropTypes.string,
+  deleteItem: PropTypes.func,
+};
